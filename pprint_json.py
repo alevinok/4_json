6cbf93aa81a968fe6ca7d98510985944affabad8
alevinok <alevinok@gmail.com>
2018-03-10 14:00:16 +0300

    normal variables

--- pprint_json.py
+++ pprint_json.py
@@ -4,16 +4,16 @@ import json, sys, os
 def load_data(filepath):
     if not os.path.isfile(filepath):            # Проверка существования файлa
         return None
-    with open(filepath, 'r', encoding="utf-8") as file:
+    with open(filepath, 'r', encoding="utf-8") as f:
         try:
-            json_content = json.load(file)
+            json_content = json.load(f)
             pretty_print_json(json_content)
         except json.decoder.JSONDecodeError:    # Не верный формат файла
             return None
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, sort_keys=True, indent=2, ensure_ascii=False))
+def pretty_print_json(data_json):
+    print(json.dumps(data_json, sort_keys=True, indent=2, ensure_ascii=False))
 
 
 if __name__ == '__main__':
